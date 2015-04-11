4c29141f35127f549b1e84b528d7e6988dbd6d23
jjm011 <jjm011@ucsd.edu>
2015-04-11 14:10:56 -0700

    problem1 solved

--- assignment1_p1.py
+++ assignment1_p1.py
@@ -1,12 +1,21 @@
 __author__ = 'jjm011@ucsd.edu,a5velasc@ucsd.edu'
 def is_complete(board):
     # your code here
+    incr = 0
+    for line in board:
+        for num in line:
+            print num
+            if num != incr:
+                return False
+            else: incr += 1
     return True
+
 def main():
     import sys
-board=[[int(n.strip()) for n in line.split(¡¯,¡¯)] for line in sys.stdin.readlines()]
-print(is_complete(board))
-if __name__ == '__main__':
+    board=[[int(n.strip()) for n in line.split(',')] for line in sys.stdin.readlines()]
+    print(is_complete(board))
+    #print board
 
-main()
+if __name__ == '__main__':
+    main()
 
